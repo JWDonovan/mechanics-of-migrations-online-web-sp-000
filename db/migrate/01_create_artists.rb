95309395264c2adc2454357a5376e0f531eaca53
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 20:15:44 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,10 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table artist do |table|
+      table.string :name
+      table.string :genre
+      table.integer :age
+      table.string :hometown
+    end
   end
 end
